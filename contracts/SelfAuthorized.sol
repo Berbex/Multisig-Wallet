49fe9d88c0c82e7b219ef8d1c46687c9216e423c
Berbex <berbesidaniel@gmail.com>
2025-05-01 18:29:42 -0400

    feat: add base SelfAuthorized contract

---
+++ contracts/SelfAuthorized.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+/**
+ * @title SelfAuthorized - Authorizes current contract to perform actions to itself
+ */
+abstract contract SelfAuthorized {
+    error NotAuthorized();
+
+    modifier authorized() {
+        if (msg.sender != address(this)) revert NotAuthorized();
+        _;
+    }
+}
